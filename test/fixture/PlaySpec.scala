d157b5d65118a13175f8474b213a1a69e45bbff9
Dmitry Ivanov <dmitry.ivanov.iamm@gmail.com>
2016-01-30 09:51:53 +0200

    Multiply test data to see it loaded for second test suite

--- test/fixture/PlaySpec.scala
+++ test/fixture/PlaySpec.scala
@@ -10,8 +10,6 @@ import slick.driver.JdbcProfile
 import org.scalatest._
 import org.scalatestplus.play.OneAppPerSuite
 
-//import DefaultDBApi._
-
 trait PlaySpec extends WordSpec
   with OneAppPerSuite
   with SequentialNestedSuiteExecution
@@ -21,33 +19,7 @@ trait PlaySpec extends WordSpec
   with HasDatabaseConfig[JdbcProfile]
   with AddressDatabaseModule {
 
-  //protected lazy val dbConfig = DatabaseConfigProvider.get[JdbcProfile](FakeApplication())
   protected lazy val dbConfig = DatabaseConfigProvider.get[JdbcProfile](Play.current)
-
-  /*lazy val appBuilder = new GuiceApplicationBuilder()
-
-  lazy val injector = appBuilder.injector()
-
-  lazy val databaseApi = injector.instanceOf[DBApi] //here is the important line*/
-  //import driver.api._
-  //defaultConnectionPool: ConnectionPool = new HikariCPConnectionPool(environment)
-  /*val conf = Map(
-    "slick.dbs.default.driver" -> "slick.driver.H2Driver$",
-    "slick.dbs.default.db.driver" -> "org.h2.Driver",
-    "slick.dbs.default.db.url" -> "jdbc:h2:mem:education-link;MODE=PostgreSQL;DB_CLOSE_ON_EXIT=FALSE",
-    "slick.dbs.default.db.user" -> "sa",
-    "slick.dbs.default.db.password" -> "",
-    "slick.dbs.default.db.logStatements" -> "true",
-    "slick.dbs.default.db.connectionTimeout" -> "30000",
-    "slick.dbs.default.db.maxConnectionsPerPartition" -> "100",
-    "slick.dbs.default.db.minConnectionsPerPartition" -> "10",
-    "slick.dbs.default.db.acquireIncrement" -> "10"
-  )
-
-  implicit lazy val fakeApp: FakeApplication = new FakeApplication(additionalConfiguration = conf)
-
-  Play.start(fakeApp) /// <--- This creates the application*/
-
 
 
 }
